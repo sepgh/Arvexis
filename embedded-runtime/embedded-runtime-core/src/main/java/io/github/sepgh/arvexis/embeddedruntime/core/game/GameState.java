6ed5d67862ce0163b56b7407d817c4daef2caa64
sepgh <13250403+sepgh@users.noreply.github.com>
2026-04-16 12:48:22 +0330

    feat: finished phase 5 of embedded runtime

--- embedded-runtime/embedded-runtime-core/src/main/java/io/github/sepgh/arvexis/embeddedruntime/core/game/GameState.java
+++ embedded-runtime/embedded-runtime-core/src/main/java/io/github/sepgh/arvexis/embeddedruntime/core/game/GameState.java
@@ -1,32 +1,12 @@
 package io.github.sepgh.arvexis.embeddedruntime.core.game;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class GameState {
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class AmbientState {
-        public String action = "stop";
-        public String zoneId;
-        public String assetRelPath;
-        public Double volume;
-        public Integer fadeMs;
-        public Boolean loop;
-    }
-
-    public String currentSceneId;
-    public Map<String, Object> variables = new HashMap<>();
-    public boolean gameOver = false;
-    public AmbientState ambient;
+public class GameState extends io.github.sepgh.arvexis.shared.game.GameState {
 
     public GameState() {
+        super();
     }
 
     public GameState(String sceneId) {
-        this.currentSceneId = sceneId;
-        this.ambient = new AmbientState();
+        super(sceneId);
     }
 }
--- runtime/src/main/java/io/github/sepgh/arvexis/runtime/game/GameState.java
+++ runtime/src/main/java/io/github/sepgh/arvexis/runtime/game/GameState.java
@@ -1,33 +1,12 @@
 package io.github.sepgh.arvexis.runtime.game;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+public class GameState extends io.github.sepgh.arvexis.shared.game.GameState {
 
-import java.util.HashMap;
-import java.util.Map;
-
-/** Mutable game state: current scene position + all state variable values. */
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class GameState {
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class AmbientState {
-        public String action = "stop";
-        public String zoneId;
-        public String assetRelPath;
-        public Double volume;
-        public Integer fadeMs;
-        public Boolean loop;
+    public GameState() {
+        super();
     }
 
-    public String currentSceneId;
-    public Map<String, Object> variables = new HashMap<>();
-    public boolean gameOver = false;
-    public AmbientState ambient;
-
-    public GameState() {}
-
     public GameState(String sceneId) {
-        this.currentSceneId = sceneId;
-        this.ambient = new AmbientState();
+        super(sceneId);
     }
 }
